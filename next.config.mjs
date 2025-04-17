e835fbbd0782750ba729329ec8f2ee6e3ab2cbad
RohitFartiyal <157445183+RohitFartiyal@users.noreply.github.com>
2025-04-17 17:29:39 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -14,19 +14,19 @@ const nextConfig = {
       },
     ],
   },
-  // async headers() {
-  //   return [
-  //     {
-  //       source: "/embed",
-  //       headers: [
-  //         {
-  //           key: "Content-Security-Policy",
-  //           value: "frame-src 'self' https://roadsidecoder.created.app;",
-  //         },
-  //       ],
-  //     },
-  //   ];
-  // },
+  async headers() {
+    return [
+      {
+        source: "/embed",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "frame-src 'self' https://roadsidecoder.created.app;",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
